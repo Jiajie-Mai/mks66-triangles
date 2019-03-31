cfb85c56a7d7eaee14e87f27a05a52c43faea57f
Jiajie <33970456+Jiajie-Mai@users.noreply.github.com>
2019-03-31 00:59:29 -0400

    did gmath

--- gmath.py
+++ gmath.py
@@ -7,23 +7,23 @@ def normalize(vector):
     factor = magnitude(vector)
     for x in range(vector):
         vector[x] /= factor
-    return vector
 
 #Return the dot porduct of a . b
 def dot_product(a, b):
     return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
 
+def cross_product(a, b):
+    return [a[1] * b[2] - b[1] * a[2], a[2] * b[0] - b[2] * a[0], a[0] * b[1] - b[0] * a[1]
+
 #Calculate the surface normal for the triangle whose first
 #point is located at index i in polygons
 def calculate_normal(polygons, i):
-    vectorU = [polygons[i + 1][0] - polygons[i][0], polygons[i + 1][1] - polygons[i][1], polygons[i + 1][2] - polygons[i][2]]
-    vectorV = [polygons[i + 2][0] - polygons[i][0], polygons[i + 2][1] - polygons[i][1], polygons[i + 2][2] - polygons[i][2]]
-
-    normalX = vectorU[1] * vectorV[2] - vectorU[2] * vectorV[1]
-    normalY = vectorU[2] * vectorV[0] - vectorU[0] * vectorV[2]
-    normalZ = vectorU[0] * vectorV[1] - vectorU[1] * vectorV[0]
-
-    return [normalX, normalY, normalZ]
+    point1 = polygons[i]
+    point2 = polygons[i + 1]
+    point3 = polygons[i + 2]
+    vectorA = [point2[0] - point1[0], point2[1] - point1[1], point2[2] - point1[2]]
+    vectorB = [point3[0] - point1[0], point3[1] - point1[1], point3[2] - point1[2]]
+    return cross_product(output)
 
 
 def magnitude(vector):
