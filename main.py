f13d9c09e70a0db300400082e6d8aecfe4689677
Jiajie <33970456+Jiajie-Mai@users.noreply.github.com>
2019-04-01 01:30:54 -0400

    finished

--- main.py
+++ main.py
@@ -10,4 +10,13 @@ edges = []
 polygons = []
 transform = new_matrix()
 
+# i = 0
+# while i < 500:
+#     add_sphere( edges, 250, 250, 250, 100 + i, 20 )
+#     i += 150
+#
+# draw_polygons(edges, screen, color)
+# display(screen)
+# save_extension(screen, args[0])
+
 parse_file( 'script', edges, polygons, transform, screen, color )
